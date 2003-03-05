92aa210c8e97fc9d6cd69e52f70766f188767949
th <th>
2003-03-05 14:14:30 +0000

    fixed evil bug where sotf_topcs_counter would be dropped but not recreated

--- node/code/classes/sotf_Repository.class.php
+++ node/code/classes/sotf_Repository.class.php
@@ -1,7 +1,7 @@
 <?php 
 
 /*  -*- tab-width: 3; indent-tabs-mode: 1; -*-
- * $Id: sotf_Repository.class.php,v 1.23 2003/03/04 10:46:47 andras Exp $
+ * $Id: sotf_Repository.class.php,v 1.24 2003/03/05 14:14:30 th Exp $
  *
  * Created for the StreamOnTheFly project (IST-2001-32226)
  * Authors: András Micsik, Máté Pataki, Tamás Déri 
@@ -215,7 +215,7 @@ class sotf_Repository {
 
   function updateTopicCounts() {
     // calculate counts by topic
-    $this->db->query("DROP TABLE sotf_topics_counter");
+    $this->db->query("DELETE FROM TABLE sotf_topics_counter");
     $this->db->query("SELECT setval('sotf_topics_counter_id_seq', 1, false)");
     $this->db->query("SELECT nextval('sotf_topics_counter_id_seq') AS id, t.id AS topic_id, count(p.id) AS number, NULL::int AS total INTO sotf_topics_counter FROM sotf_topic_tree_defs t LEFT JOIN sotf_prog_topics p ON t.id = p.topic_id GROUP BY t.id");
     // calculate totals including subtopic counts
