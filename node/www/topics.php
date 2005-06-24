4f70cbca001111813e3131b7c61547b3e73cde87
wreutz <wreutz>
2005-06-24 14:33:00 +0000

    changed all line endings of text files from pc-style(crlf) to unix-style(lf)

--- node/www/topics.php
+++ node/www/topics.php
@@ -1,15 +1,29 @@
 <?php  // -*- tab-width: 3; indent-tabs-mode: 1; -*- 
 
+
+
 /*  
- * $Id: topics.php,v 1.4 2003/03/05 09:11:40 andras Exp $
+
+ * $Id: topics.php,v 1.5 2005/06/24 14:33:06 wreutz Exp $
+
  * Created for the StreamOnTheFly project (IST-2001-32226)
+
  * Authors: András Micsik, Máté Pataki, Tamás Déri 
+
  *          at MTA SZTAKI DSD, http://dsd.sztaki.hu
+
  */
+
+
 
 require("init.inc.php");
 
+
+
 $smarty->assign("openTree", sotf_Utils::getParameter("open"));
+
 $smarty->display("topics.htm");
+
+
 
 ?>
